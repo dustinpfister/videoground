c738fb895d402917e6ea7aa2b3df468e15aea2f4
dustinpfister <dustin.pfister@gmail.com>
2022-07-26 16:04:16 -0400

    video13 removed white space

--- start-videos/video13.js
+++ start-videos/video13.js
@@ -3,35 +3,17 @@
  *       
  */
 VIDEO.init = function(sm, scene, camera){
-
     // can add to sm object
-
-    var width = 854,
-    height = 480;
     sm.cams = [
-        new THREE.PerspectiveCamera(40, width / height, 0.1, 1000),
-        new THREE.PerspectiveCamera(60, width / height, 0.1, 1000),
+        new THREE.PerspectiveCamera(40, 854 / 480, 0.1, 1000),
+        new THREE.PerspectiveCamera(60, 854 / 480, 0.1, 1000),
         new THREE.OrthographicCamera( -5, 5, 2, -2, 1, 1000 )
     ];
-
-    //sm.camera = sm.cams[1];
-    //sm.camera.position.set(5, 5, 5);
-    //sm.camera.lookAt(0, 0, 0);
-
     // GRID HELPER
     scene.add( new THREE.GridHelper(8, 8) );
-    
-
 };
 VIDEO.update = function(sm, scene, camera, per, bias){
-
-
     sm.camera = sm.cams[ Math.floor(sm.cams.length * per) ];
-
     sm.camera.position.set(5 - 10 * bias, 5 - 5 * bias, 5);
     sm.camera.lookAt(0, 0, 0);
-
-
-
 };
-
