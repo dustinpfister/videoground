501c48e0a64413b3031dcd2a85d8fc0e566d8a9c
dustinpfister <dustin.pfister@gmail.com>
2022-07-28 14:07:12 -0400

    added plane and spotlight in video14

--- start-videos/video14.js
+++ start-videos/video14.js
@@ -4,12 +4,29 @@
  */
 VIDEO.init = function(sm, scene, camera){
     // GRID HELPER
-    scene.add( new THREE.GridHelper(8, 8) );
+    //scene.add( new THREE.GridHelper(8, 8) );
+
+    // LIGHT
+    var dl = new THREE.DirectionalLight(0xffffff, 0.1);
+    dl.position.set(1,1,-1);
+    scene.add(dl);
+    var sl = new THREE.SpotLight(0xffffff, 0.5);
+    sl.position.set(1, 1,-1);
+    scene.add(sl);
+
 
     // CUSTOM RENDERER
     var renderer = new THREE.WebGL1Renderer();
     renderer.shadowMap.enabled = true; // shadow map enabled
     sm.replaceRenderer(renderer);      // replace
+
+    // PLANE MESH
+    var plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(5, 5, 10, 10),
+        new THREE.MeshStandardMaterial()
+    );
+    plane.geometry.rotateX(Math.PI * 1.5)
+    scene.add(plane);
 
 
 };
