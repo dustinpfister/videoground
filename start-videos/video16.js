26e43f75d68e3f2f6164cf4827279d9b31c541a3
dustinpfister <dustin.pfister@gmail.com>
2022-08-15 16:35:21 -0400

    video16 looks better

--- start-videos/video16.js
+++ start-videos/video16.js
@@ -2,21 +2,21 @@
  *     
  *       
  */
-
-
 VIDEO.init = function(sm, scene, camera){
-    // GRID HELPER
-    scene.add( new THREE.GridHelper(8, 8) );
-
+    // BACKGROUND
+    scene.background = new THREE.Color(0, 0.5, 0.5);
+    // LIGHT
+    var dl = new THREE.DirectionalLight(0xffffff, 1);
+    dl.position.set(3, 2, 1);
+    scene.add(dl);
+    // returning a promise that must resolve or reject to continue
     return new Promise(function(resolve, reject){
         const loader = new THREE.BufferGeometryLoader();
-
         // load a resource
         loader.load(
             // resource URL
              //'json/foo.json',
-             videoAPI.pathJoin(sm.filePath, 'json/foo.json'),
-
+             videoAPI.pathJoin(sm.filePath, 'json/box_house1_solid.json'),
              // onLoad callback
              function ( geometry ) {
                   resolve(geometry);
@@ -30,21 +30,20 @@ VIDEO.init = function(sm, scene, camera){
                  reject(err)
              }
         );
-
     }).catch(function(err){
-
         console.log(err.message); // failed to fetch message
         console.log(videoAPI);
-
     }).then(function(geometry){
-
         console.log('we should have geo');
         console.log(geometry);
-        var mesh = new THREE.Mesh(geometry);
+        // createing a mesh with it
+        var mesh = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial());
+        mesh.geometry.rotateX(Math.PI * 1.5);
+        mesh.geometry.rotateY(Math.PI * 1.0);
         scene.add(mesh);
-
     });
 };
-
 VIDEO.update = function(sm, scene, camera, per, bias){
+    camera.position.set(15 - 30 * per, 0, 15);
+    camera.lookAt(0, 0, 0);
 };
