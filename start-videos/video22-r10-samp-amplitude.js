f2ed29f4943965f58cd0c3236002b98fef2cae44
dustinpfister <dustin.pfister@gmail.com>
2023-08-18 13:55:29 -0400

    for sample option

--- start-videos/video22-r10-samp-amplitude.js
+++ start-videos/video22-r10-samp-amplitude.js
@@ -1,36 +1,35 @@
 /*    video21-r10-start - start video for r10 of videoground
+          * based on what I started with video21 which was just a simple sin wave tone
+          * can use custom logic for amplitude and frequency
  */
 //-------- ----------
 // CREATE SINE POINTS METHIOD
 //-------- ----------
-// create sine points array
+// create sine points 4 method that allows for applying custom logic for 
+// both frequency and amplitude
 const create_sine_points_4 = ( opt = {} ) => {
     const i_size = opt.i_size === undefined ? 20 : opt.i_size;
     const i_start = opt.i_start === undefined ? 8 : opt.i_start;
     const i_count = opt.i_count === undefined ? 8 : opt.i_count;
-    //const frequency = opt.frequency === undefined ? 1 : opt.frequency;
     const secs = opt.secs === undefined ? 1 : opt.secs;
-
-    //const amplitude = opt.amplitude === undefined ? 0.75 : opt.amplitude;
-
     const mode = opt.mode === undefined ? 'bytes' : opt.mode;
-    const step = opt.step === undefined ? 1 : opt.step; 
+    const step = opt.step === undefined ? 1 : opt.step;
+    // what to do for the sample settings object each time
+    const for_sample = opt.for_sample || function( samp_set ){
+        return samp_set;
+    };
     const sine_points = [];
-
+    let samp_set = {
+        amplitude: 1,
+        frequency: 80
+    };
     const i_end = i_start + i_count;
     let i = i_start;
     while(i < i_end){
         const a_point = i / i_size;
-
-        const a_pp = THREE.MathUtils.pingpong(a_point * 16, 1);
-
-        const amplitude = 0.20 + (0.75 * a_point) * THREE.MathUtils.smoothstep( a_pp, 0, 1);
-        const frequency = 80 + 200 * a_point;
-
-        const wave_count = frequency * secs;
-
-
-        let samp = Math.sin( Math.PI * 2 * wave_count * a_point )  * amplitude;
+        samp_set = for_sample(samp_set, i, a_point);
+        const wave_count = samp_set.frequency * secs;
+        let samp = Math.sin( Math.PI * 2 * wave_count * a_point )  * samp_set.amplitude;
         if(mode === 'bytes'){
             let byte = Math.round( 127.5 + 128 * samp );
             samp = THREE.MathUtils.clamp(byte, 0, 255);
@@ -44,6 +43,13 @@ const create_sine_points_4 = ( opt = {} ) => {
     }
     return sine_points;
 };
+const for_sample_one = ( samp_set, i, a_point ) => {
+    const a_amp = THREE.MathUtils.smootherstep(a_point, 0, 1);
+    samp_set.amplitude = 0.05 + 0.45 * a_amp;
+    const a_freq = Math.sin( Math.PI * (a_point * 4 % 1) );
+    samp_set.frequency = 400 - 320 * a_freq;
+    return samp_set;
+};
 //-------- ----------
 // DRAW SAMPLE DATA - AND RELATED METHODS
 //-------- ----------
@@ -53,21 +59,17 @@ const getsamp_lossy_random = (sample_array, i, index_step) => {
     const samp = sample_array[i + i_delta];
     return samp;
 };
-const getsamp_lossy_pingpong = (function(){
-    let high = 0;
-    return ( sample_array, i, index_step ) => {
-        const frame_samps = sample_array.slice( i, i + index_step );
-        const a = Math.max.apply(null, frame_samps);
-        const b = Math.min.apply(null, frame_samps);
-        let samp = b;
-        if(high){
-            samp = a;
-        }
-        high += 1;
-        high %= 2;
-        return samp;
-    };
-}());
+const getsamp_lossy_pingpong = ( sample_array, i, index_step ) => {
+    const high = i % 2;
+    const frame_samps = sample_array.slice( i, i + index_step );
+    const a = Math.max.apply(null, frame_samps);
+    const b = Math.min.apply(null, frame_samps);
+    let samp = b;
+    if(high){
+        samp = a;
+    }
+    return samp;
+};
 const draw_sample_data = (ctx, sample_array, opt = {} ) => {
     const sx = opt.sx === undefined ? 0 : opt.sx;
     const sy = opt.sy === undefined ? 0 : opt.sy;
@@ -141,10 +143,10 @@ VIDEO.init = function(sm, scene, camera){
     sm.frameMax = sine.frames;
     const total_bytes = sine.sample_rate * sine.secs;
     sine.array_disp = create_sine_points_4({
+        for_sample: for_sample_one,
         i_size: total_bytes,
         i_start:0,
         i_count: total_bytes,
-        frequency: sine.frequency,
         secs: sine.secs,
         mode: 'raw'
     });
@@ -160,12 +162,11 @@ VIDEO.update = function(sm, scene, camera, per, bias){
     const total_bytes = sine.sample_rate * sine.secs;
     const i_start = sine.bytes_per_frame * sm.frame;
     const data_samples =  sine.array_frame = create_sine_points_4({
+        for_sample: for_sample_one,
         i_size : total_bytes,
         i_start : i_start,
         i_count : sine.bytes_per_frame,
-        frequency: sine.frequency,
         secs: sine.secs,
-        amplitude: sine.amplitude,
         mode: 'bytes'
     });
     // write data_samples array
@@ -176,7 +177,7 @@ VIDEO.update = function(sm, scene, camera, per, bias){
 //-------- ----------
 // RENDER
 //-------- ----------
-const opt_disp = { w: 1280 - 50 * 2, h: 250, sy: 100, sx: 50, getsamp_lossy: getsamp_lossy_random };
+const opt_disp = { w: 1280 - 50 * 2, h: 250, sy: 100, sx: 50, getsamp_lossy: getsamp_lossy_pingpong };
 const opt_frame = { w: 1280 - 50 * 2, h: 250, sy: 400, sx: 50, mode: 'bytes' };
 VIDEO.render = function(sm, canvas, ctx, scene, camera, renderer){
     const sine = scene.userData.sine;
